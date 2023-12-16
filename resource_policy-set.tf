48e65f998433843a9b68d888d73dd4295b9426a4
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-16 10:53:11 -0500

    ft: policy-set

---
+++ resource_policy-set.tf
@@ -0,0 +1,8 @@
+resource "tfe_policy_set" "test" {
+  name          = "Global-Policy-Set"
+  description   = "This policy-set is assigned at the organization level."
+  organization  = data.tfe_organization.this.name
+  global        = true
+  kind          = "sentinel"
+  policy_ids    = [tfe_policy.allowed-providers.id]
+}
