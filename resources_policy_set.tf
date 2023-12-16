26b5ab092ea433016d6828693c2ba9813b2b915e
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-16 09:52:03 -0500

    slug

--- resources_policy_set.tf
+++ resources_policy_set.tf
@@ -1,13 +1,13 @@
 data "tfe_slug" "global" {
   // point to the local directory where the policies are located.
-  source_path = "my-policy-set"
+  source_path = "policy-set"
 }
 
 resource "tfe_policy_set" "global" {
   name          = "Global-Policy-Set"
-  description   = "This policy-set is assigned at the organization level."
-  global        = true
-  organization  = data.tfe_organization.this.name
+  description  = "This policy-set is assigned at the organization level."
+  global       = true
+  organization = data.tfe_organization.this.name
   // reference the tfe_slug data source.
   slug = data.tfe_slug.global
 }
