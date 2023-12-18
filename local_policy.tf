9b086055b335933fcc8b5468bfcd5e0a575df14d
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-18 12:24:19 -0500

    ft: policy

---
+++ local_policy.tf
@@ -0,0 +1,4 @@
+locals {
+  # This local is used to get a list of all folder inside the policies folder.
+  files = fileset("${local.policies_folder}", "*/*.sentinel")
+}
--- resources_policy.tf
+++ resources_policy.tf
@@ -4,6 +4,6 @@ resource "tfe_policy" "this" {
   # description  = ""
   organization = data.tfe_organization.this.name
   kind         = "sentinel"
-  policy       = file("policies/${each.key}")
+  policy       = file("${local.policies_folders}/${each.key}")
   enforce_mode = "advisory" # advisory, hard-mandatory and soft-mandatory
 }
