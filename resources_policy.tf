a3bddda9680af21002043db13dc46a12db77d04c
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-18 12:05:21 -0500

    ft: fileset

--- resources_policy.tf
+++ resources_policy.tf
@@ -1,6 +1,7 @@
 resource "tfe_policy" "this" {
   for_each = local.files
-  name     = element(split(".", element(split("/", each.key), length(each.key) - 1)), 2)
+  name     = element(split("/", each.key), length(each.key) - 1)
+  # name     = element(split(".", element(split("/", each.key), length(each.key) - 1)), 2)
   # description  = ""
   organization = data.tfe_organization.this.name
   kind         = "sentinel"
