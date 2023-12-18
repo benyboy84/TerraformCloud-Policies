67a205c78627986f9e91e62a9205bd0a653136b7
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-18 15:03:54 -0500

    ft: policy

--- local_policy.tf
+++ local_policy.tf
@@ -1,4 +1,4 @@
 locals {
   # This local is used to get a list of all folder inside the policies folder.
-  files = fileset("${local.policies_folder}", "*/*.sentinel")
+  files = fileset(local.policies_folder, "*/*.sentinel")
 }
