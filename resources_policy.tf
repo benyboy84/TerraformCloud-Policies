0c6c5fe73a54e9def9f7871c12e6775b04fe7187
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-18 12:25:15 -0500

    ft: policy

--- resources_policy.tf
+++ resources_policy.tf
@@ -4,6 +4,6 @@ resource "tfe_policy" "this" {
   # description  = ""
   organization = data.tfe_organization.this.name
   kind         = "sentinel"
-  policy       = file("${local.policies_folders}/${each.key}")
+  policy       = file("${local.policies_folder}/${each.key}")
   enforce_mode = "advisory" # advisory, hard-mandatory and soft-mandatory
 }
