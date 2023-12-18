904b48786964d38aacf3c1bc2005cf14cfd848ab
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-12-18 11:59:55 -0500

    ft: fileset

--- resources_policy.tf
+++ resources_policy.tf
@@ -1,9 +1,9 @@
 resource "tfe_policy" "allowed-providers" {
   for_each = local.files
-  name     = element(split(".", element(split("/", each.key), length(each.key)-1)), 1)
+  name     = element(split(".", element(split("/", each.key), length(each.key)-1)), 2)
   # description  = "This policy uses the tfconfig/v2 import to restrict providers to those in an allowed list."
   organization = data.tfe_organization.this.name
   kind         = "sentinel"
-  policy       = file("policies/global/allowed-providers.sentinel")
+  policy       = file(each.key)
   enforce_mode = "advisory" # advisory, hard-mandatory and soft-mandatory
 }
