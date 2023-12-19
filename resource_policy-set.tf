003ffeb5bb71e5c5aa9ec3110ab40dff54f3a471
GitHub Action <notifications@github.com>
2023-12-19 16:32:23 +0000

    Automated Terraform Formatting

--- resource_policy-set.tf
+++ resource_policy-set.tf
@@ -1,5 +1,5 @@
 resource "tfe_policy_set" "test" {
-  name   = "Global-Policy-Set"
+  name         = "Global-Policy-Set"
   description  = "This policy-set is assigned at the organization level."
   organization = data.tfe_organization.this.name
   global       = true
